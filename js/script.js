973600faeb889a128eebc79bff1314196f146ee4
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-09-11 18:58:20 +0200

    Feat: add img avatar

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Creazione array di oggetti contenenti persone facenti parte del teem di lavoro
+
+const team = [
+    {
+      foto: "img/foto_1.jpg",
+      nome: "Marco Rossi",
+      ruolo: "Front-End Developer",
+      competenze: ["HTML", "CSS", "JavaScript", "React"],
+      esperienza: 3 // anni di esperienza
+    },
+    {
+      nome: "Sara Bianchi",
+      ruolo: "Back-End Developer",
+      competenze: ["Node.js", "Express", "MongoDB", "SQL"],
+      esperienza: 5
+    },
+    {
+      nome: "Luca Verdi",
+      ruolo: "Full Stack Developer",
+      competenze: ["React", "Node.js", "GraphQL", "PostgreSQL"],
+      esperienza: 4
+    },
+    {
+      nome: "Elena Neri",
+      ruolo: "UI/UX Designer",
+      competenze: ["Figma", "Adobe XD", "Sketch", "CSS"],
+      esperienza: 6
+    },
+    {
+      nome: "Davide Rossi",
+      ruolo: "DevOps Engineer",
+      competenze: ["AWS", "Docker", "Kubernetes", "CI/CD"],
+      esperienza: 7
+    },
+    {
+      nome: "Giulia Fontana",
+      ruolo: "Project Manager",
+      competenze: ["Agile", "Scrum", "Kanban", "JIRA"],
+      esperienza: 8
+    }
+  ];
+  
+  
